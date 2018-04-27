3403eae4630c68605587e158334fb9a8dc28ede3
MikhailMaksimovStetson <mikemaksimov@yahoo.com>
2018-04-27 19:43:44 +0000

    Added methods that put events into database (not all working)

--- rssdaemon/parser.cpp
+++ rssdaemon/parser.cpp
@@ -1,5 +1,6 @@
 #include "pugixml.hpp"
 #include <iostream>
+#include "../database.h"
 
 using namespace std;
 
@@ -8,35 +9,40 @@ int main()
 	pugi::xml_document doc;
 	doc.load_file("cultural-credits.rss");
 	pugi::xml_node root = doc.child("rss").child("channel");
+	database test;
 	const string NC = "\e[0m";
 	const string BOLD = "\e[1m";
 	const string RED = "\e[38;5;196m";
-	std::cout << std::endl;
 
 	for (pugi::xml_node item = root.child("item"); item; item = item.next_sibling("item"))
 	{
 		string title = item.child("title").child_value();
+		if (title.find('*') < 1)
+		{
+			title = title.substr(1,title.length()-1);
+		}
 		string description = item.child("description").child_value();
 		string pubdate = item.child("pubDate").child_value();
 		string link = item.child("link").child_value();
 		string guid = item.child("guid").child_value();
 		
-		/*
+		
 		cout << RED << "title: " << NC << BOLD << title << NC << endl;
 		if (description!="")
 		{	
-			cout << RED << "description: " << NC << description << endl;
+			//cout << RED << "description: " << NC << description << endl;
 		} else 
 		{
-			cout << RED << "no description" << NC << endl;
+			//cout << RED << "no description" << NC << endl;
 		}
-		cout << RED << "pubDate: " << NC << pubdate << endl;
-		cout << RED << "link: " << NC << link << endl;
-		cout << RED << "guid: " << NC << guid << endl;
+		//cout << RED << "pubDate: " << NC << pubdate << endl;
+		//cout << RED << "link: " << NC << link << endl;
+		//cout << RED << "guid: " << NC << guid << endl;
 		cout << endl << endl;
-		*/
+		
 
-		//insertEventData(title, pubdate, "no location", description);
+		//test.insertEventData(title, pubdate, "no location", description);
 	}
-	cout << "data inserted" << endl;
+	//test.searchByName("Global Perspectives on Economic Development");
+	//cout << "data inserted" << endl;
 }
