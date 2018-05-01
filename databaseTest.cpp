0fa8c1a7435e95fa99ea967a62a2334a151b20d4
Kyle Burda <kburda@stetson.edu>
2018-05-01 22:09:39 +0000

    Made get studentByEmail work

--- databaseTest.cpp
+++ databaseTest.cpp
@@ -11,7 +11,9 @@ int main()
         //db.insertEventData("Lunch", "12:00", "the commons", "A nice meal");
         //db.insertEventData("Dinner", "6:00", "the commons", "A bigger meal");
 	vector<Student> v = db.searchStudentByEmail("email@email.com");
-	cout << v[0].getName() + " " + v[0].getEmail() + " " << v[0].getId() << " " << v[0].getCurrentCC() << endl;
+	cout << v[0].getName() + " " + v[0].getEmail() + " ";
+	cout << v[0].getId() << endl;
+	cout << v[0].getCurrentCC() << endl;
         return 0;
 }
 
