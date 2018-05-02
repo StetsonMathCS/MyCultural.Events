4af661f8a00de28ec15df6b736fc39020df63a82
Heyley Gatewood <hgatewood@stetson.edu>
2018-05-02 15:15:32 -0400

    Search and Replace with text files.

--- emaildaemon/test.cpp
+++ emaildaemon/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "../student.h"
 #include "../ccevent.h"
 #include "../database.h"
@@ -18,18 +19,50 @@ using namespace std;
  * 	8.) get template from Michael
  * 	9.) send to Nick and Isabel
  */
+
 /*
-void find_and_replace(std::string& file_contents,const std::string& morn, const std::string& night) {
-	auto pos = file_contents.find(morn);
-	while (pos != std::string::npos) {
-		file_contents.replace(pos, morn.length(), night);
-		pos = file_contents.find(morn, pos);
+void find_and_replace(string& file_contents,const string& f, const string& r) {
+	string pos = file_contents.find(f);
+	while (pos != string::npos) {
+		file_contents.replace(pos, f.length(), r);
+		pos = file_contents.find(f, pos);
 	}
 
 }
+
+string getfile(ifstream& is) {
+  string contents;
+  for (char ch; is.get(ch); contents.push_back(ch)) {}
+  return contents;
+}
 */
 
-int main(int argc, const char **argv){
+void setStudentNameInEmail(ifstream& in, ofstream& out, Student student){
+	string line;
+        string wordToReplace = "!!FirstName!!";
+        size_t len = wordToReplace.length();
+                        while (getline(in, line)){
+                                while (true){
+                                        size_t pos = line.find(wordToReplace);
+                                        if (pos != string::npos){
+                                                line.replace(pos, len, "Heyley");
+						//break;
+                                        	out << line << endl;
+					}else{
+						out << line << endl;
+                                                break;
+                                        }
+
+                                        //out << line << endl;
+                        	}
+				
+				//out << line << endl;
+			}	
+			//out << line << endl;
+}
+
+
+int main(int argc, const char** argv){
 	if (argc !=2)
 	{
 		cout << "usage: " << argv[0] << " databasename.db" << endl;
@@ -39,26 +72,63 @@ int main(int argc, const char **argv){
 	//db.insertStudentData("Heyley", "hgatewood@stetson.edu", 2020, "Fall", 10, "art");
 	//db.insertStudentData("John", "johndoe@stetson.edu", 2019, "Fall", 8, "music, film");
 
+	ifstream filein("textfile.txt");
+	ofstream fileout("temp.txt");
+	if(!filein || !fileout) //if both files are not available
+	{
+        	cout << "Error opening files!" << endl;
+        	return 1;
+    	}
+
+
 	int numStudentRows = db.rowsInStudentTable();
-	cout << numStudentRows << endl;
-
-	
-	for(int i = 0; i < 2; i++){
-		vector<Student> stvec = db.searchStudentById(i);	
-		Student st = stvec[0];
-		st.setPriorityLevel();
-		int priorityLevel = st.getPriorityLevel();
+        cout << numStudentRows << endl;
+	Student st;
+	CCEvent e;
+        for(int i = 0; i < 1; i++){
+                vector<Student> stvec = db.searchStudentById(i);
+                st = stvec[0];
+                st.setPriorityLevel();
+                int priorityLevel = st.getPriorityLevel();
+                cout << "Student's name is: " << st.getName() << endl;
 		cout << "Student's priority level has been set to: " << priorityLevel << endl;
-		if(priorityLevel > 0){ //HIGH PRIORITY
-			//for(int j = 0; j < db.rowsInEventTable(); j++){
-			vector<CCEvent> ccvec = db.searchEventById(1);
-			CCEvent e = ccvec[0];
-			cout << "Date/Time for this event is: " << e.getDateTime() << endl;				
-			
-			//}
-		}
+                //if(priorityLevel > 0){
+                        for(int j = 0; j < db.rowsInEventTable(); j++){
+				vector<CCEvent> ccvec = db.searchEventById(1);
+        	                e = ccvec[0];
+                	        cout << "Date/Time for this event is: " << e.getDateTime() << endl;
+			//Chooses Cultural Credit for that student
+                        }
+			//Setting the Student's First name in the email template
+			setStudentNameInEmail(filein, fileout, st);
+/*			string line;
+    			string wordToReplace = "!!FirstName!!";
+    			size_t len = wordToReplace.length();
+    			while (getline(filein, line)){
+        			while (true){
+            				size_t pos = line.find(wordToReplace);
+            				if (pos != string::npos)
+                				line.replace(pos, len, "GOODBYE");
+            				else 
+                				break;
+        				}	
 
+        				fileout << line << '\n';
+    			}*/
+			
+		//}
+		//}
 	}
+	
+	//Settin the Event's Time in the email template
+
+
+
+	//find_and_replace(contents, "HELLO", "GOODBYE");
+	//string contents = getfile(file);
+	//find_and_replace(contents, "HELLO", "GOODBYE");
+	//fileout << contents;
+
 /*
 	vector<CCEvent> v = db.searchEventById(1);
 	CCEvent e = v[0];
