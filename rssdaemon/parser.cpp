5dfe609774722862dc3aa4c4c91d8c311a1f55a7
MikhailMaksimovStetson <mikemaksimov@yahoo.com>
2018-05-02 14:17:24 +0000

    parser can now download location and rss feed and input items into database

--- rssdaemon/parser.cpp
+++ rssdaemon/parser.cpp
@@ -1,5 +1,6 @@
 #include "pugixml.hpp"
 #include <iostream>
+#include <stdlib.h>
 #include "../database.h"
 #include "../ccevent.h"
 
@@ -7,10 +8,13 @@ using namespace std;
 
 int main()
 {
-	pugi::xml_document doc;
-	doc.load_file("cultural-credits.rss");
-	pugi::xml_node root = doc.child("rss").child("channel");
+	system("curl -o feed.rss https://www.stetson.edu/programs/calendar/rss/cultural-credits.rss");
+	pugi::xml_document RSSfeed;
+	RSSfeed.load_file("feed.rss");
+	//RSSfeed.load_file("cultural-credits.rss");
+	pugi::xml_node root = RSSfeed.child("rss").child("channel");
 	database db;
+	
 	const string NC = "\e[0m";
 	const string BOLD = "\e[1m";
 	const string RED = "\e[38;5;196m";
@@ -18,19 +22,20 @@ int main()
 	for (pugi::xml_node item = root.child("item"); item; item = item.next_sibling("item"))
 	{
 		string title = item.child("title").child_value();
-		if (title.find('*')!=string::npos)
-		{
-			for (unsigned int i = 0; i < title.length(); i++)
-
-				title = title.substr(1,title.length()-1);
-		}
 		string description = item.child("description").child_value();
 		string pubdate = item.child("pubDate").child_value();
 		string link = item.child("link").child_value();
 		string guid = item.child("guid").child_value();
 		
-		
-		//cout << RED << "title: " << NC << BOLD << title << NC << endl;
+		string curl = "curl -o tmp.html ";
+		string command = curl.append(link);
+		system(command.c_str());
+		pugi::xml_document eventPage;
+		eventPage.load_file("tmp.html");
+		string location = eventPage.child("html").child("body").child("div").child("article").child("div").last_child().child_value();
+		system("rm tmp.html");
+
+		cout << RED << "title: " << NC << BOLD << title << NC << endl;
 		if (description!="")
 		{	
 			//cout << RED << "description: " << NC << description << endl;
@@ -39,10 +44,12 @@ int main()
 			//cout << RED << "no description" << NC << endl;
 		}
 		//cout << RED << "pubDate: " << NC << pubdate << endl;
+		//cout << location << endl;
 		//cout << RED << "link: " << NC << link << endl;
 		//cout << RED << "guid: " << NC << guid << endl;
 		//cout << endl << endl;
-		/*
+		
+		
 		bool hasEvent = false;
 		for (int i = 1; i <= db.rowsInEventTable(); i++)
 		{
@@ -50,7 +57,7 @@ int main()
 			if (vector.size() != 0)
 			{
 				CCEvent event = vector[0];
-				if (title == event.getTitle())
+				if (title == event.getTitle() && pubdate == event.getDateTime())
 				{
 					hasEvent = true;
 				}
@@ -58,16 +65,13 @@ int main()
 		}
 		if (hasEvent == false)
 		{
-			db.insertEventData(title, pubdate, "no location", description);
+			db.insertEventData(title, pubdate, location, description);
 		}
-		*/
 	}
 	
-			
+	cout << endl << endl << endl;
 	for (int i = 1; i <= db.rowsInEventTable(); i++)
 	{
 		db.searchEventById(i);
-		cout << i << endl;
 	}
-	cout << db.rowsInEventTable() << " rows" << endl;
 }
