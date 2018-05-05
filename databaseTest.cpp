d0ef872392ff0ac62ff2267f62fdbb6de1e414bf
Mohammed Batarfi <mbatarfi@stetson.edu>
2018-05-05 14:48:17 -0400

    fixed duplicated events

--- databaseTest.cpp
+++ databaseTest.cpp
@@ -12,16 +12,17 @@ int main(int argc, const char **argv)
 		return -1;
 	}
         database db(argv[1]);
-        db.insertStudentData("kyle", "email@email.com", 15, "Fall", 2018, "Music");
-        db.insertEventData("Lunch", "12:00", "the commons", "A nice meal");
-        db.insertEventData("Dinner", "6:00", "the commons", "A bigger meal");
-	cout << db.rowsInStudentTable() << endl;
+	//db.insertStudentData("kyle", "email@email.com", 15, "Fall", 2018, "Music");
+        //db.insertEventData("Lunch", "12:00", "the commons", "A nice meal");
+        db.insertEventData("Dinner", "7:00", "the commons", "A bigger meal");
+	//cout << db.rowsInStudentTable() << endl;
 	//vector<Student> v = db.searchStudentById(1);
+        cout << db.rowsInEventTable() << endl;
 	//cout << v[0].getName() + " " + v[0].getEmail() + " ";
-	vector<CCEvent> v = db.searchEventByDesc("A bigger meal");
-	cout << v[0].getId() << endl;
-	cout << v[0].getTitle() + " " + v[0].getDateTime() + " ";
-	cout << v[0].getLocation() + " " + v[0].getDescription() << endl;
+	//vector<CCEvent> v = db.searchEventByDesc("A bigger meal");
+	//cout << v[0].getId() << endl;
+	//cout << v[0].getTitle() + " " + v[0].getDateTime() + " ";
+	//cout << v[0].getLocation() + " " + v[0].getDescription() << endl;
     
     return 0;
 }
